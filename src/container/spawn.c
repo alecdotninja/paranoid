1070821012510ec36d469c05084c7b2c7f6e6ca6
Alec Larsen <alec@unabridgedsoftware.com>
2017-07-03 00:53:22 -0500

    Vendorizes libcap

--- src/container/spawn.c
+++ src/container/spawn.c
@@ -13,7 +13,7 @@ pid_t __attribute__((noinline)) spawn(int (*main) (void *), int flags, void *arg
 void container_spawn_child(container_t *container, int (*container_main) (container_t *container)) {
     void *child_data = (void *)container;
 
-    pid_t child_pid = spawn(container_main, CLONE_NEWCGROUP | CLONE_NEWNET | CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWUSER | CLONE_NEWPID, child_data);
+    pid_t child_pid = spawn((int (*)(void *))container_main, CLONE_NEWCGROUP | CLONE_NEWNET | CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWUSER | CLONE_NEWPID, child_data);
 
     if(child_pid < 0) {
         fprintf(stderr, "[!] Cannot spawn child process. Perhaps your kernel does not support namespaces?\n");
